4b1b423181e2312f68e597772eb4f099b0423a71
jiale0329 <68922276+jiale0329@users.noreply.github.com>
2022-05-30 14:08:54 +0800

    delete books

---
+++ public/delete_books_execute.php
@@ -0,0 +1,50 @@
+<?php
+
+    session_start ();
+
+    // Declaring function for alert message //
+    function alert ($msg)
+    {
+
+        echo "<script type='text/javascript'>alert('$msg'); window.location='check-stock.php';</script>";
+
+    }
+
+    // Gathering user input //
+    $isbn = $_POST ['isbn'];
+
+    // Validating the input //
+    if (empty ($isbn))
+    {
+
+        alert ("Error Please Check Your ISBN!");
+
+    }
+    else 
+    {
+
+         // Establishing connection to database //
+        $connection = mysqli_connect ('localhost', 'root', '');
+
+        mysqli_select_db ($connection, 'lastrow_bookstore');
+
+        $sql = "DELETE FROM stock WHERE isbn ='$isbn'";
+
+        if($result = mysqli_query ($connection, $sql))
+        {
+            header ("Location: check-stock.php");
+        }
+        else 
+        {
+
+            alert ("Delete Book Failed!");
+
+        }
+
+        mysqli_close ($connection);
+
+    }
+
+   
+
+?>
