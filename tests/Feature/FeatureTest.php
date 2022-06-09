03b933e8a0883a42b2eea1c3b2f5a70088989c26
Eric9546 <ericmok85@gmail.com>
2022-06-09 17:48:29 +0800

    Sprint 2 Unit Tests

--- tests/Feature/FeatureTest.php
+++ tests/Feature/FeatureTest.php
@@ -20,32 +20,4 @@ class FeatureTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_visitor_view_books_desc_prices()
-    {
-        $response = $this->get('/index.php');
-
-        $response->assertStatus(200);
-    }
-
-    public function test_visitor_search()
-    {
-        $response = $this->get('/index.php');
-
-        $response->assertStatus(200);
-    }
-
-    public function test_customer_view_books()
-    {
-        $response = $this->get('/index.php');
-
-        $response->assertStatus(200);
-    }
-
-    public function test_admin_view_customer_payment_history()
-    {
-        $response = $this->get('/customer-payment-history.php');
-
-        $response->assertStatus(200);
-    }
-
 }
---
+++ tests/Feature/Sprint2Test.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class Sprint2Test extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+
+    public function test_visitor_view_books_desc_prices_page()
+    {
+        $response = $this->get('/index.php');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_visitor_search_page()
+    {
+        $response = $this->get('/index.php');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_customer_view_books_page()
+    {
+        $response = $this->get('/index.php');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_admin_view_customer_payment_history_page()
+    {
+        $response = $this->get('/customer-payment-history.php');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_admin_edit_books_page()
+    {
+        $response = $this->get('/check-stock.php');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_admin_delete_books_page()
+    {
+        $response = $this->get('/check-stock.php');
+
+        $response->assertStatus(200);
+    }
+
+}
